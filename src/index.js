e51f61b9baadbef56571579bc2144f3ee828fab3
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-24 12:06:20 -0700

    install redux-form

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers /* preloadedState, */,
+  composeEnhancers(applyMiddleware())
+);
+// const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,8 +21,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
 import { combineReducers } from 'redux';
+// built into redux form
+import { reducer as formReducer } from 'redux-form';
+
 import authReducer from './authReducer';
 
-export default combineReducers({ auth: authReducer });
+export default combineReducers({ auth: authReducer, form: formReducer });
